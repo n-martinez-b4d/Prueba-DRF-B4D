cadda2c151d03ad4fa8f5df1be878a4906cbc3af
garyfernandez <jotagary25@protonmail.com>
2024-05-28 19:17:40 -0400

    fix: atributte AUTHOR addesde to POST model

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 # for post model
@@ -7,6 +7,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     state = models.BooleanField(default=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
