d09a85bc98ebf6ff89b64ab887dd994162c1bb76
garyfernandez <jotagary25@protonmail.com>
2024-05-28 21:17:05 -0400

    feat: postgres container created, and conection done

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myprojectDB',
+        'USER': 'garyFernandez',
+        'PASSWORD': 'gary123123',
+        'HOST': 'localhost',
+        'PORT': '8765',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
