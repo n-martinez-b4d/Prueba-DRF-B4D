348b11c99fc3a3fa00b332f1938655be7436c353
garyfernandez <jotagary25@protonmail.com>
2024-05-28 21:33:59 -0400

    feat: serializers for POST model created

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
 # for post model
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    state = models.BooleanField(default=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
